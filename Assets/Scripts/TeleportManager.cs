d9b8e00a69fdce2a8ee9d6ec66ea1871609c0004
VlasBochkarev <vlas.bochkarev@gmail.com>
2022-07-01 12:36:05 +0300

    first commit

---
+++ Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    public GameObject portal;
+
+
+
+    private void Start()
+    {
+        portal.GetComponent<Transform>();
+    }
+
+    public void TeleportToPortal()
+    {
+        float portalPosX = portal.transform.position.x;
+        float portalPosZ = portal.transform.position.z;
+        transform.position = new Vector3(portalPosX, 20f, portalPosZ);
+    }
+    public void OnPointerClick()
+    {
+        TeleportToPortal();
+    }
+
+ 
+}
